68717edc16f088ea60429313ae9a597606a8db21
saiteja-mandadi-99 <saitejamandadi992gmail.com>
2024-09-02 08:24:30 +0000

    basic code without any features

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,38 @@
-// Write your code here
+import './index.css'
+import {formatDistanceToNow} from 'date-fns'
+
+const CommentItem = props => {
+  const {commentDetails} = props
+  const {name, comment} = commentDetails
+  const initial = name.slice(0, 1).toUpperCase() // Corrected from splice to slice
+
+  return (
+    <li>
+      <div className="commentHeading">
+        <div className="commentHeadingInitial">
+          <p>{initial}</p>
+        </div>
+        <h1 className="commentUserName">{name}</h1>
+        <p className="dateEle">{formatDistanceToNow(new Date())}</p>
+      </div>
+      <p className="userComment">{comment}</p>
+      <div className="like-delete-container">
+        <div className="likeImage">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+            alt="Like"
+            className="like"
+          />
+          <p className="likeTagSelect">Like</p>
+        </div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+          alt="delete"
+          className="delete"
+        />
+      </div>
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,3 +1,7 @@
+import {Component} from 'react'
+import './index.css'
+import CommentItem from '../CommentItem'
+import {v4 as uuidv4} from 'uuid'
 const initialContainerBackgroundClassNames = [
   'amber',
   'blue',
@@ -9,3 +13,56 @@ const initialContainerBackgroundClassNames = [
 ]
 
 // Write your code here
+
+class Comments extends Component {
+  state = {commentsList: [], count: 0, name: '', comment: ''}
+  render() {
+    const {commentsList, count} = this.state
+    return (
+      <div className="appContainer">
+        <div className="commentsContainer">
+          <h1 className="heading">Comments</h1>
+          <div className="addComments">
+            <form className="inputComments">
+              <p className="labelEle">Say something about 4.0 Technologies</p>
+              <input
+                id="comments"
+                placeholder="Your Name"
+                className="inputEle"
+              />
+              <textarea
+                rows="8"
+                cols="50"
+                placeholder="Your Comment"
+                className="textareaEle"
+              ></textarea>
+              <button type="submit" className="buttonEle">
+                Add Comment
+              </button>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              alt="comments"
+              className="imageEle"
+            />
+          </div>
+
+          <hr className="hrEle" />
+          <div className="commentsDetailsContainer">
+            <p>{count}Comments</p>
+            <ul className="listContainer">
+              {commentsList.map(eachComment => (
+                <CommentItem
+                  key={eachComment.id}
+                  commentDetails={eachComment}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comments
